a184becc19289b373d4092446fa4234d980082b3
Shameemmd <shameemmd1707@gmail.com>
2026-03-02 11:21:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+model = joblib.load("alzheimers_small_model.pkl")
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    input_data = np.array([features])
+    prediction = model.predict(input_data)
+
+    result = "Alzheimer's Disease" if prediction[0] == 1 else "Normal"
+
+    return render_template("index.html", prediction_text=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
